df2bffe539fbf19d52a55c0e39ebd48bfce22b62
pedrovmlopes <pedrovitormlopes@gmail.com>
2024-03-14 19:42:29 -0300

    Atualização Inicial do Projeto

---
+++ assets/js/pokemonInfo.js
@@ -0,0 +1,11 @@
+const pokemonInfo = document.getElementById('pokemonInfo');
+
+function pokemonInformation(pokemonInfo) {
+    return `
+    <a href="index.html">
+        <button>Voltar</button>
+    </a>
+    <h1>Nome do Pokemon</h1>
+
+    `
+}
